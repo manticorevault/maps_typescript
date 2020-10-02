b346fcb37062143bfd57aca774fe8b56af29d158
manticorevault <artur_seilah@hotmail.com>
2020-10-02 16:12:55 +0100

    Created and exported the User class

---
+++ src/User.ts
@@ -0,0 +1,19 @@
+// Import faker and
+// Install the Type Definition File from @type/faker
+import faker from "faker";
+
+export class User {
+	name: string;
+	location: {
+		lat: number;
+		lng: number;
+	};
+
+	constructor() {
+		this.name = faker.name.firstName();
+		this.location = {
+			lat: parseFloat(faker.address.latitude()),
+			lng: parseFloat(faker.address.longitude()),
+		};
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("It's working!")
+import { User } from "./User";
+
+const user = new User();
+
+console.log(user);
