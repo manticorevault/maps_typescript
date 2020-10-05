9d94d857b20f1c9b126b8cfdc7ddc8d1e670a9bb
manticorevault <artur_seilah@hotmail.com>
2020-10-05 20:19:54 +0100

    Installed Type Google

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -100489,6 +100489,7 @@ var user = new User_1.User();
 var company = new Company_1.Company();
 console.log(user);
 console.log(company);
+google;
 },{"./User":"src/User.ts","./Company":"src/Company.ts"}],"../../../.nvm/versions/node/v12.18.3/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -100517,7 +100518,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44655" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42519" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
