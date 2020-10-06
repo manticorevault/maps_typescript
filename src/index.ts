a30140026137270523d4b29a44fd08c4e35847c0
manticorevault <artur_seilah@hotmail.com>
2020-10-06 14:46:00 +0100

    Added the CustomMap class, imported and displayed the Map in the browser

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 import { User } from "./User";
 import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 
-const user = new User();
-const company = new Company();
-
-console.log(user);
-console.log(company);
-
-google;
+new CustomMap("map");
