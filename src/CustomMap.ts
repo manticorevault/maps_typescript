7fc30981dc2258f84c855e5c364a2de02630b926
manticorevault <artur_seilah@hotmail.com>
2020-10-06 19:25:10 +0100

    Changed the addMarker function to take both User and Company as an argument

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,16 +1,26 @@
-export class CustomMap {
-	private googleMap: google.maps.Map;
+import { User } from './User'
+import { Company } from './Company'
 
-	constructor(divId: string) {
-		this.googleMap = new google.maps.Map(
-			document.getElementById(divId),
-			{
-				zoom: 1,
-				center: {
-					lat: 0,
-					lng: 0,
-				},
-			}
-		);
-	}
+export class CustomMap {
+  private googleMap: google.maps.Map
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    })
+  }
+
+  addMarker(mappable: User | Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    })
+  }
 }
