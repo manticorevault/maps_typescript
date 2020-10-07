8a8fad83783e4ea30d8230ce2fb9b47198b05ac0
manticorevault <artur_seilah@hotmail.com>
2020-10-07 14:20:31 +0100

    Exported the Mappable interface and imported it to Company and User to implement it

--- src/User.ts
+++ src/User.ts
@@ -1,13 +1,16 @@
 // Import faker and
 // Install the Type Definition File from @type/faker
 import faker from 'faker'
+import { Mappable } from './CustomMap'
 
-export class User {
+export class User implements Mappable {
   name: string
   location: {
     lat: number
     lng: number
   }
+
+  color: string = 'red'
 
   constructor() {
     this.name = faker.name.firstName()
